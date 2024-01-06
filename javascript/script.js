b3992369bd0b0bccbd509ffce462c6f7937cad59
ferrywilana <ferrywilana1@gmail.com>
2024-01-06 14:57:13 +0700

    memberikan style pada button,shopping cart

--- javascript/script.js
+++ javascript/script.js
@@ -17,10 +17,18 @@ document.querySelector("#search-button").onclick = (e) => {
   e.preventDefault();
 };
 
+/* toggle class active untuk shopping cart */
+const shoppingcart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingcart.classList.toggle("active");
+  e.preventDefault();
+};
+
 // klik diluar elemen
 
 const hm = document.querySelector("#hamburger-menu");
 const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (e) {
   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -30,4 +38,22 @@ document.addEventListener("click", function (e) {
   if (!sb.contains(e.target) && !searchFrom.contains(e.target)) {
     searchFrom.classList.remove("active");
   }
+  if (!sc.contains(e.target) && !shoppingcart.contains(e.target)) {
+    shoppingcart.classList.remove("active");
+  }
 });
+
+/* Modal box */
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButton = document.querySelector(".item-detail-button");
+
+itemDetailButton.onclick = (e) => {
+  itemDetailModal.style.display = "flex";
+  e.preventDefault();
+};
+
+// klik tombol close
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
